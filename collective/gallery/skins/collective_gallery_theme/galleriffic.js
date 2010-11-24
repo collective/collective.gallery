98afe7e0c0818120ae635239909420f3f62a29f0
jmf <jmf@localhost.(none)>
2010-11-24 11:45:03 +0100

    add collective.js.galleriffic as develop in default buildout. Use jq instead of $

--- collective/gallery/skins/collective_gallery_theme/galleriffic.js
+++ collective/gallery/skins/collective_gallery_theme/galleriffic.js
@@ -1,12 +1,12 @@
 // We only want these styles applied when javascript is enabled
-$(document).ready(function(){
-    $('div.navigation').css({
+jq(document).ready(function(){
+    jq('div.navigation').css({
         'width': '300px',
         'float': 'left'
     });
-    $('div.gallerycontent').css('display', 'block');
+    jq('div.gallerycontent').css('display', 'block');
     // Initialize Minimal Galleriffic Gallery
-    $('#thumbs').galleriffic({
+    jq('#thumbs').galleriffic({
         imageContainerSel: '#slideshow',
         controlsContainerSel: '#controls'
     });
--- collective/gallery/skins/collective_gallery_theme/gallery.js
+++ collective/gallery/skins/collective_gallery_theme/gallery.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-    $("#pikame").PikaChoose({text: { play: "", stop: "", previous: "", next: "" }});
-    $("#pikame").jcarousel({
+jq(document).ready(function(){
+    jq("#pikame").PikaChoose({text: { play: "", stop: "", previous: "", next: "" }});
+    jq("#pikame").jcarousel({
         scroll: 4,
         initCallback: function(carousel){
-            $(carousel.list).find('img').click(function(){
+            jq(carousel.list).find('img').click(function(){
                 //console.log($(this).parents('.jcarousel-item').attr('jcarouselindex'));
                 carousel.scroll(parseInt($(this).parents('.jcarousel-item').attr('jcarouselindex')));
             });

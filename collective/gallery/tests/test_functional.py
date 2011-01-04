9bfeb68c7f6718b79eae458385a5c4fe111be092
toutpt <toutpt@gmail.com>
2011-01-04 11:38:03 +0100

    use config variable for base functional class tests

--- collective/gallery/tests/test_functional.py
+++ collective/gallery/tests/test_functional.py
@@ -7,22 +7,24 @@ from Testing import ZopeTestCase as ztc
 from collective.gallery.tests import base
 
 def test_suite():
+    TEST_CLASS = base.FunctionalTestCase
     OPTIONFLAGS = (doctest.REPORT_ONLY_FIRST_FAILURE | 
                         doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS)
     return unittest.TestSuite([
 
         ztc.FunctionalDocFileSuite(
             'topic.txt', package='collective.gallery',
-            test_class=base.FunctionalTestCase,
+            test_class=TEST_CLASS,
             optionflags=OPTIONFLAGS),
 
         ztc.FunctionalDocFileSuite(
             'folder.txt', package='collective.gallery',
-            test_class=base.FunctionalTestCase,
+            test_class=TEST_CLASS,
             optionflags=OPTIONFLAGS),
 
         ztc.FunctionalDocFileSuite(
             'link/link.txt', package='collective.gallery',
-            test_class=base.FunctionalTestCase,
+            test_class=TEST_CLASS,
             optionflags=OPTIONFLAGS),
+
         ])

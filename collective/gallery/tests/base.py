d28717201a098f360b478dcbe0780a0f7ced07b0
toutpt <toutpt@gmail.com>
2011-01-04 11:38:55 +0100

    remove setup add from base.py

--- collective/gallery/tests/base.py
+++ collective/gallery/tests/base.py
@@ -1,24 +1,6 @@
-"""Test setup for integration and functional tests."""
-
-import sys
-from Products.Five import zcml
-from Products.Five import fiveconfigure
-
-from Testing import ZopeTestCase as ztc
 from Products.PloneTestCase import PloneTestCase as ptc
-from Products.PloneTestCase.layer import onsetup
 
 from collective.gallery.tests import layer
-
-#@onsetup
-#def setup_addon():
-#    fiveconfigure.debug_mode = True
-#    import collective.gallery
-#    zcml.load_config('configure.zcml', collective.gallery)
-#    fiveconfigure.debug_mode = False
-#    ztc.installPackage('collective.gallery')
-
-#setup_addon()
 
 ptc.setupPloneSite(extension_profiles=['collective.gallery:default'])
 

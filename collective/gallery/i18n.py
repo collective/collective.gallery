a3477a603cc374344eae3a3ea025aa208b975966
toutpt <toutpt@gmail.com>
2011-01-05 15:17:41 +0100

    update translations

--- collective/gallery/i18n.py
+++ collective/gallery/i18n.py
@@ -1,5 +1,5 @@
 from collective.gallery import messageFactory as _
 
 gallery_view_title = _(u"Gallery view")
-message_no_backend_for_link = _(u"message_no_photos",
-                      default=u"Gallery ERROR: No backend has been found for this link. Please check it or contact the add-on author: toutpt at gmail dot com")
+message_no_backend_for_link = _(u"message_no_backend_for_link",
+                      default=u"Gallery ERROR: This link is not supported.")

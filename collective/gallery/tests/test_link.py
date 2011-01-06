5200769fc4db052054cb7b92ae7fa06ef79b03fa
toutpt <toutpt@gmail.com>
2011-01-06 11:49:03 +0100

    add integration tests on Link gallery view.

--- collective/gallery/tests/test_link.py
+++ collective/gallery/tests/test_link.py
@@ -1,6 +1,8 @@
 import unittest
+from collective.gallery.tests import base
 from collective.gallery.tests import utils
 from zope.publisher.browser import TestRequest as Request
+from zope import component
 
 class Test(unittest.TestCase):
 
@@ -23,10 +25,26 @@ class Test(unittest.TestCase):
     def testCreator(self):
         self.failUnless(self.view.creator == self.view.context.Creators()[0])
 
+class TestIntegration(base.TestCase):
+
+    def afterSetUp(self):
+        self.folder.invokeFactory(id='mylink', type_name='Link')
+        self.link = self.folder.mylink
+        self.link.setRemoteUrl('http://notsupported.com/agallery')
+
+    def testProperties(self):
+#        view = component.getMultiAdapter((self.link, self.link.REQUEST),
+#                                         name="gallery")
+        view = self.link.unrestrictedTraverse('@@gallery')
+        self.failUnless(view.width == 400)
+        self.portal.portal_properties.gallery_properties.photo_max_size = 500
+        self.failUnless(view.width == 500)
+
 def test_suite():
     """This sets up a test suite that actually runs the tests in the class
     above
     """
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(Test))
+    suite.addTest(unittest.makeSuite(TestIntegration))
     return suite
